ca7973ced3a9be3abe9e7b91ad92ce9186db1f73
sbuteryd <sbuteryd@qq.com>
2020-10-29 23:27:28 +0800

    cart

--- jquery/JQuery_day02/self.js
+++ jquery/JQuery_day02/self.js
@@ -25,4 +25,36 @@ $(function(){
             $('.cart-item').removeClass('check-cart-item')
         }
     })
+    $('.increment').click(function(){
+        var n = $(this).siblings('.itxt').val()
+        n++
+        $(this).siblings('.itxt').val(n)
+        // 计算价格
+        let p = $(this).parents('.p-num').siblings('.p-price').html()
+         p = p.substr(1);
+         let price = (p*n).toFixed(2)
+       $(this).parents('.p-num').siblings('.p-sum').html(`￥${price}`);
+     })
+    $('.decrement').click(function(){
+        let n = $(this).siblings('.itxt').val()
+        if(n==1){
+            return false
+        }
+        n--
+        $(this).siblings('.itxt').val(n)
+        // 得到当前的总价格
+        let p = $(this).parents('.p-num').siblings('.p-price').html()
+         // 商品单价
+         p = p.substr(1)
+    
+         $(this).parents('.p-num').siblings('.p-sum').html((p * n).toFixed(2));
+
+    })
+
+$('.itxt').change(function(){
+    var n = $(this).val()
+    let p = $(this).parents('.p-num').siblings('.p-price').html()
+    p = p.substr(1)
+    $(this).parents(".p-num").siblings(".p-sum").html("￥" + (p * n).toFixed(2));
+})
 })
